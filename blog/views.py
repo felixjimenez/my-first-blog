31a995a26b6f4c62439b9e62f249b9ca130abb06
felix <fvjpsg@gmail.com>
2015-10-07 21:26:00 -0430

    agrega vista para crear/editar  blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,5 @@ from .models import Post
 # Create your views here.
 
 def post_list(request):
-    post = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'post':post})
+    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
